43819477f9336b68e53570fc154344e65c06d90f
burns <john.piatras@gmail.com>
2018-05-23 12:56:06 +0100

    Added functions for add, subtract, multiply, divide

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,2 +1,19 @@
 "use strict"
 
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+
