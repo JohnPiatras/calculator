81eb19ab74e3d4c6a6da6f492965cb644ebfcb84
burns <john.piatras@gmail.com>
2018-05-24 14:01:10 +0100

    Basic calculator functionality is in place

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,66 +1,126 @@
 "use strict"
 
-let buttons = document.querySelectorAll("button");
-buttons.forEach(button => {
-    button.addEventListener('click', onClick);
-
-});
-
-function onClick(e){
-    console.log("Button = " + e.target.textContent);
-    let button_txt = e.target.textContent;
-    let num = Number(button_txt);
-
-    if(!isNaN(num)){
-        console.log(" a number!");
-        onNumberClick(num);
-    }
-}
-
+//Globals
 let display_value = 0;
+let display_stale = false;  //true if showing previous result - its gone stale, so clear and start again when  user presses a digit
+let operands = [];
+let operator = null;
+
 updateDisplay();
 
-//limit to 15 digits?
-function onNumberClick(num){
+let buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+    if(button.textContent.match(/[0123456789]/g))
+        button.addEventListener('click', onClickNumber);
+    else if(button.textContent.match(/[*+-\/]/g)){
+        button.addEventListener('click', onClickOperator);
+    }else if(button.textContent == "="){
+        button.addEventListener('click', onClickEquals);
+    }else if(button.textContent == "AC"){
+        button.addEventListener('click', onClickAllClear);
+    }else if(button.textContent == "C"){
+        button.addEventListener('click', onClickClear);
+    }    
+});
+
+function onClickNumber(e){
+    let num = Number(e.target.textContent);
+    if(display_stale){
+        display_value = 0;
+        display_stale = false;
+    }
     if( ("" + display_value).length < 15){
         display_value = (10 * display_value) + num;
-        console.log("Current display value = " + display_value);
         updateDisplay();
     }else {
         console.log("Digit limit reached");
     }
 }
 
-function updateDisplay(){
-    document.getElementById("number-display").textContent = "" + display_value;
-
-}
-
-let calculator = {
-    operands: [],
-    operator: null,
-
-    operate: function(a, b, op){
-        let r;
-        switch(op){
-            case '+':
-                r = a + b;  
-                break;
-            case '-':
-                r = a - b;
-                break;
-            case "*":
-                r = a * b;
-                break;
-            case "/":
-                r = a / b;
-                break;
-        }
-        return r;
+function onClickOperator(e){
+    let op = e.target.textContent;
+    
+    if(!display_stale){
+        operands.push(display_value);
+        display_stale = true;
     }
 
-
-
+    if(operands.length == 2){
+        operands.push( operate());
+        display_value = operands[0];
+        display_stale = true;
+    }
+    operator = op;
+    updateDisplay();
 }
+
+function onClickEquals(){
+    if(operands.length == 1 && operator != null && !display_stale){
+        operands.push(display_value);
+        operands.push(operate());
+        display_value = operands[0];
+        operator = null;
+        display_stale = true;
+    } 
+
+    updateDisplay();
+}
+
+function allClear(){
+    display_value = 0;
+    display_stale = false;
+    operands = [];
+    operator = null;
+}
+
+function onClickAllClear(){
+    allClear();
+    updateDisplay();
+}
+
+function onClickClear(){
+    if(display_stale){
+        allClear();
+    }else
+        display_value = Math.floor(display_value / 10);
+    updateDisplay();
+}
+
+function updateDisplay(){
+    document.getElementById("number-display").textContent = "" + display_value;
+    document.getElementById("op-display").textContent = operator != null ? operator: "";
+}
+
+function printstate(){
+    console.log("---State---");
+    console.log("    operands: " + operands);
+    console.log("    operator: " + operator);
+    console.log("    display: " + display_value + ", stale: " + display_stale);
+}
+
+
+
+
+function operate(){
+    let r;
+    let a = operands.shift();
+    let b = operands.shift();
+    switch(operator){
+        case '+':
+            r = a + b;  
+            break;
+        case '-':
+            r = a - b;
+            break;
+        case "*":
+            r = a * b;
+            break;
+        case "/":
+            r = a / b;
+            break;
+    }
+    return r;
+}
+
 
 
